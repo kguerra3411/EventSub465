eb9e56885c79867e5a9f33b705246014ac873a73
josh <joshuaholman5@gmail.com>
2025-03-08 00:17:40 -0800

    terraform init

---
+++ mediawiki-aws/variables.tf
@@ -0,0 +1,35 @@
+variable "aws_region" {
+  description = "AWS region to deploy resources"
+  type = string
+  default = "us-west-1"
+}
+
+variable "project_name" {
+  description = "Name of the project"
+  type = string
+  default = "mediawiki"
+}
+
+variable "environment" {
+  description = "Deployment environment"
+  type = string
+  default = "dev"
+}
+
+variable "db_username" {
+  description = "Database username"
+  type = string
+  sensitive = true
+}
+
+variable "db_password" {
+  description = "Database password"
+  type = string
+  sensitive = true
+}
+
+variable "db_name" {
+  description = "Database name"
+  type = string
+  default = "mediawiki"
+}
