6fbc4b4ac33d3a0125e0872c8e3632b4dcc77196
josh <joshuaholman5@gmail.com>
2025-03-11 22:53:03 -0700

    fixed var syntax

--- mediawiki-aws/main.tf
+++ mediawiki-aws/main.tf
@@ -33,9 +33,9 @@ resource "aws_db_instance" "wiki_db" {
   engine = "mysql"
   instance_class = "db.t3.micro"
   identifier = "wiki"
-  db_name = "${db_name}"
-  username = "${db_username}"
-  password = "${db_password}"
+  db_name = var.db_name
+  username = var.db_username
+  password = var.db_password
   port = "3306" #can be whatever
   #once we have security groups and subnets set up in the vpc we'd add them here i think
   #vpc_security_group_ids = [aws_security_group.rds_sg.id]
