c3348724a3b2bca8c0cd27832a20fba1ee2dfcbb
josh <joshuaholman5@gmail.com>
2025-05-02 21:36:39 -0700

    fix domain (should be efs)

--- mediawiki-aws/transfer_family.tf
+++ mediawiki-aws/transfer_family.tf
@@ -3,6 +3,8 @@ resource "aws_transfer_server" "transfer_family" {
   identity_provider_type = "SERVICE_MANAGED"
   logging_role           = aws_iam_role.transfer_logging.arn
   protocols              = ["SFTP"]
+  domain                 = "EFS"
+
 
   tags = {
     Name        = "${var.project_name}-transfer-server"
